da642f3234931ecc9aa22619a0414a473b5f2608
XiaoXu <331538695@qq.com>
2020-06-02 22:29:51 +0800

    channel细节

--- base-grammar/channel/channelTest/main.go
+++ base-grammar/channel/channelTest/main.go
@@ -1,60 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
 
 func main() {
+	//测试使用协程耗时
+	start := time.Now().Nanosecond()
 	//开启第一个协程存8000数
 	go putNum(numChan)
 	//开启8个协程操作
-	for i := 0; i < 8; i++ {
+	for i := 0; i < numCpu; i++ {
 		go operation(numChan, resultChan, exitChan)
 	}
 	//开启一个匿名协程关闭resultChan
 	go func() {
-		for i := 0; i < 8; i++ {
-			v := <-exitChan
-			fmt.Println("exitChan-->", v)
+		for i := 0; i < numCpu; i++ {
+			<-exitChan
 		}
-		fmt.Println(len(resultChan))
+		end := time.Now().Nanosecond()
+		fmt.Println(start - end)
 		close(resultChan)
 	}()
+	//打印结果
 	for {
-		num, ok := <-resultChan
+		_, ok := <-resultChan
 		if !ok {
 			break
 		}
-		fmt.Println(num)
+		//fmt.Println(num)
 	}
+	fmt.Println(numCpu)
 }
 
 var (
+	//获取cpu数量
+	numCpu = runtime.NumCPU()
 	//存放数的管道
 	numChan = make(chan int, 1000)
 	//存放结果的管道
 	resultChan = make(chan int, 2000)
 	//存放标志管道,退出管道，
-	exitChan = make(chan bool, 8)
+	exitChan = make(chan bool, numCpu)
 )
 
 func putNum(numChan chan int) {
-	fmt.Println("putNum function start")
 
 	for i := 1; i <= 8000; i++ {
 		numChan <- i
 	}
-	fmt.Println("numChan length is ", len(numChan))
 	close(numChan)
-	fmt.Println("putNum function exit")
 }
 func operation(numChan chan int, resultChan chan int, exitChan chan bool) {
-	fmt.Println("operation function start")
 	for {
 		v, ok := <-numChan
 		if !ok {
 			break
 		}
 		flag := true
-		for i := 1; i <= v; i++ {
+		for i := 2; i < v; i++ {
 			if v%i == 0 {
 				flag = false
 				break
@@ -65,5 +71,6 @@ func operation(numChan chan int, resultChan chan int, exitChan chan bool) {
 		}
 	}
 	exitChan <- true
-	fmt.Println("operation exit")
+	//打印消息是很耗时的
+	//fmt.Println("operation exit")
 }
